28132661c1f3ee7b783656803387c73d85f98421
XE-RO <delgadox0@gmail.com>
2023-08-14 12:43:08 -0600

    homework 1 check

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function BinarioADecimal(num) {}
 
-function DecimalABinario(num) {}
+function BinarioADecimal(num) {
+   let array1=num.toString().split('').reverse()
+   let resultado=0
+   for(let i=0 ; i < array1.length ; i++){
+      resultado+=Math.pow(2,i)*array1[i]
+   }
+return resultado
+
+}
+
+function DecimalABinario(num) {
+   let binario = []
+   while(num>=1){
+      binario.unshift(num%2)
+      num=Math.floor(num/2)
+   }
+   return binario.join("")
+   
+}
 
 module.exports = {
    BinarioADecimal,
