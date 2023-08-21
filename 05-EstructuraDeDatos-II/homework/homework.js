c184757235ee44d023738691c1d6d10ceb7e4571
XE-RO <delgadox0@gmail.com>
2023-08-21 07:41:27 -0600

    Homework 5 check

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { has } = require("markdown-it/lib/common/utils");
+
 /* EJERCICIO 1
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
@@ -89,14 +91,62 @@ Nuetra tabla hash, internamente, consta de un arreglo de buckets (slots, contene
 Para este ejercicio, la tabla debe tener 35 buckets (numBuckets = 35). (Luego de haber pasado todos los tests, a modo de ejercicio adicional, pueden modificar un poco la clase para que reciba la cantidad de buckets por parámetro al momento de ser instanciada.)
 
 La clase debe tener los siguientes métodos:
+
   - hash: función hasheadora que determina en qué bucket se almacenará un dato. Recibe un input alfabético, suma el código numérico de cada caracter del input (investigar el método charCodeAt de los strings) y calcula el módulo de ese número total por la cantidad de buckets; de esta manera determina la posición de la tabla en la que se almacenará el dato.
+
   - set: recibe el conjunto clave valor (como dos parámetros distintos), hashea la clave invocando al método hash, y almacena todo el conjunto en el bucket correcto.
+
   - get: recibe una clave por parámetro, y busca el valor que le corresponde en el bucket correcto de la tabla.
+
   - hasKey: recibe una clave por parámetro y consulta si ya hay algo almacenado en la tabla con esa clave (retorna un booleano).
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() {}
+function HashTable() {
+  this.array=[]
+  this.numBuckets=35
+}
+
+HashTable.prototype.hash=function(key){
+  let hash=0
+  for(let i=0 ; i<key.length ; i++){
+  hash+=key.charCodeAt(i)
+  }
+  return hash % this.numBuckets
+}
+
+HashTable.prototype.set=function(key,value){
+  if(typeof key !="string")throw TypeError('Keys must be string')
+
+  let index=this.hash(key)
+
+if(!this.array[index]){
+  this.array[index]={}
+}
+  this.array[index][key]=value
+
+}
+
+HashTable.prototype.get=function(key){
+  let index=this.hash(key)
+  return this.array[index][key]
+}
+HashTable.prototype.hasKey=function(key){
+  let index=this.hash(key)
+
+  return !!this.array[index][key]
+ 
+}
+
+// let nueva=new HashTable()
+// nueva.set("xavier","hola")
+// nueva.set("xavier",10) 
+// console.log(nueva.hash("xavier"))
+// console.log(nueva.get("xavier"))
+// console.log(nueva.haskey("xavier"))
+// console.log(nueva)
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
